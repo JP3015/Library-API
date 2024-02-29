9af3e4cafa66d0f7d79498dfb4a0781d056c1668
JP3015 <jpotoni02@gmail.com>
2024-02-29 11:44:07 -0300

    Initial Commit

---
+++ src/main/java/com/api/library/LibraryApplication.java
@@ -0,0 +1,21 @@
+package com.api.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class LibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryApplication.class, args);
+	}
+	
+	@GetMapping("/")
+	public String index() {
+		return "Olá Mundo";
+	}
+
+}
