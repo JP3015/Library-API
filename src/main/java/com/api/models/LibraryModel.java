281b1adc31107dce92e8c8c18c0d6d256ca72ec1
JP3015 <jpotoni02@gmail.com>
2024-02-29 12:10:58 -0300

    Criação do models

---
+++ src/main/java/com/api/models/LibraryModel.java
@@ -0,0 +1,86 @@
+package com.api.models;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TB_BOOKS")
+public class LibraryModel implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    @Column(nullable = false, unique = true, length = 70)
+    private String bookName;
+    @Column(nullable = false, length = 70)
+    private String author;
+    @Column(nullable = false, length = 35)
+    private String genre;
+    @Column(nullable = false, precision = 6)
+    private Integer pages;
+    @Column(nullable = false, precision = 6, scale = 2)
+    private Float value;
+    @Column(nullable = false)
+    private LocalDateTime registrationDate;
+    
+    
+	public UUID getId() {
+		return id;
+	}
+	public void setId(UUID id) {
+		this.id = id;
+	}
+	public String getBookName() {
+		return bookName;
+	}
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getGenre() {
+		return genre;
+	}
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	public Integer getPages() {
+		return pages;
+	}
+	public void setPages(Integer pages) {
+		this.pages = pages;
+	}
+	public Float getValue() {
+		return value;
+	}
+	public void setValue(Float value) {
+		this.value = value;
+	}
+	public LocalDateTime getRegistrationDate() {
+		return registrationDate;
+	}
+	public void setRegistrationDate(LocalDateTime registrationDate) {
+		this.registrationDate = registrationDate;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+    
+    
+    
+}
